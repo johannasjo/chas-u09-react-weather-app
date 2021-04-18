8b13e8e3885d79ae56a962e722f52b3e2b507b47
Johanna Sjöstrand <johanna.sjoestrand@gmail.com>
2021-04-18 19:51:49 +0200

    add context making location global

--- src/App.js
+++ src/App.js
@@ -10,79 +10,25 @@ import {
 import Weather from './components/Weather/Weather';
 import DisplayHeader from './components/Header/DisplayHeader';
 import LongtermWeather from './components/Weather/LongtermWeather/LongtermWeather';
+import { LocationContextProvider } from './context/LocationContext';
+import { CityContextProvider } from './context/CityContext';
 
 function App() {
-  const [loadingState, setLoadingState] = useState(null);
-  const [currentWeatherState, setCurrentWeatherState] = useState(null);
-  const [longtermWeatherState, setLongtermWeatherState] = useState(null);
-  const [locationState, setLocationState] = useState(null);
-
-  // get geolocation
-  function getPosition() {
-    // convert non-promised based API ti promise-based
-    return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(resolve, reject);
-    });
-  }
-
-  // convert time for sunrise/sunset
-  function convertEpochToLocaleTime(epochTime) {
-    return new Date(parseInt(epochTime) * 1000).toLocaleTimeString();
-  }
-
-  // use geolocation to fetch weather for that position
-  useEffect(() => {
-    setLoadingState(true);
-    getPosition()
-      .then((pos) => {
-        console.log({ currentPosition: pos });
-        const { latitude: lat, longitude: lon } = pos.coords;
-        return { lat, lon };
-      })
-      // get keys from the incoming object
-      .then(({ lat, lon }) => {
-        fetch(
-          `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
-        )
-          .then((response) => {
-            return response.json();
-          })
-          .then((apiReply) => {
-            console.log({ currentWeatherApiReply: apiReply });
-            setCurrentWeatherState(formatWeatherData(apiReply));
-            setLoadingState(false);
-          });
-      });
-  }, []);
-
-  const iconUrl = 'http://openweathermap.org/img/wn/';
-
-  const formatWeatherData = (apiReply) => ({
-    name: apiReply.name,
-    country: apiReply.sys.country,
-    id: apiReply.id,
-    temp: Math.floor(apiReply.main.temp),
-    humidity: apiReply.main.humidity,
-    windSpeed: apiReply.wind.speed,
-    sunrise: convertEpochToLocaleTime(apiReply.sys.sunrise),
-    sunset: convertEpochToLocaleTime(apiReply.sys.sunset),
-    icon: `${iconUrl}${apiReply.weather[0].icon}.png`,
-    main: apiReply.weather[0].main,
-  });
-
   return (
-    <Router>
-      <DisplayHeader />
-      <Switch>
-        <Route exact path="/">
-          <Redirect to={`/today`} />{' '}
-        </Route>
-        <Route path="/today">
-          {currentWeatherState && <Weather weather={currentWeatherState} />}
-        </Route>
-        <Route path="/longterm" component={LongtermWeather}></Route>
-      </Switch>
-    </Router>
+    <LocationContextProvider>
+      <CityContextProvider>
+        <Router>
+          <DisplayHeader />
+          <Switch>
+            <Route exact path="/">
+              <Redirect to={`/today`} />{' '}
+            </Route>
+            <Route path="/today" component={Weather}></Route>
+            <Route path="/longterm" component={LongtermWeather}></Route>
+          </Switch>
+        </Router>
+      </CityContextProvider>
+    </LocationContextProvider>
   );
 }
 
