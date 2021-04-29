c6bc9120f51bb4a08ac52878abce0672f0d1fa38
johannasjo <johanna.sjoestrand@gmail.com>
2021-04-29 16:00:20 +0200

    add spinner

--- src/components/Weather/LongtermWeather/LongtermWeather.js
+++ src/components/Weather/LongtermWeather/LongtermWeather.js
@@ -3,8 +3,9 @@ import { useCityContext } from '../../../context/CityContext';
 import { useLocationContext } from '../../../context/LocationContext';
 import { useTemperatureContext } from '../../../context/TemperatureContext';
 import Accordion from '../../Accordion/Accordion';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-const LongtermWeather = (props) => {
+const LongtermWeather = props => {
   const [loadingState, setLoadingState] = useState(null);
   const [longtermWeatherState, setLongtermWeatherState] = useState(null);
   const cityContext = useCityContext();
@@ -17,20 +18,20 @@ const LongtermWeather = (props) => {
   function convertEpochToLocaleTime(epochTime) {
     return new Date(parseInt(epochTime) * 1000).toLocaleTimeString([], {
       hour: '2-digit',
-      minute: '2-digit',
+      minute: '2-digit'
     });
   }
 
   function convertEpochToLocaleDate(epochTime) {
     return new Date(parseInt(epochTime) * 1000).toLocaleDateString([], {
-      day: '2-digit',
+      day: '2-digit'
     });
   }
 
   const iconUrl = 'http://openweathermap.org/img/wn/';
 
-  const formatDailyWeatherData = (apiReply) => {
-    return apiReply.daily.map((item) => ({
+  const formatDailyWeatherData = apiReply => {
+    return apiReply.daily.map(item => ({
       id: item.dt,
       dateTime: convertEpochToLocaleDate(item.dt),
       temp: Math.floor(item.temp.day),
@@ -39,19 +40,19 @@ const LongtermWeather = (props) => {
       icon: `${iconUrl}${item.weather[0].icon}.png`,
       main: item.weather[0].main,
       sunrise: convertEpochToLocaleTime(item.sunrise),
-      sunset: convertEpochToLocaleTime(item.sunset),
+      sunset: convertEpochToLocaleTime(item.sunset)
     }));
   };
 
-  const formatHourlyWeatherData = (apiReply) => {
-    return apiReply.list.map((item) => ({
+  const formatHourlyWeatherData = apiReply => {
+    return apiReply.list.map(item => ({
       id: item.dt,
       dateTime: convertEpochToLocaleTime(item.dt),
       temp: Math.floor(item.main.temp),
       humidity: item.main.humidity,
       windSpeed: item.wind.speed,
       icon: `${iconUrl}${item.weather[0].icon}.png`,
-      main: item.weather[0].main,
+      main: item.weather[0].main
     }));
   };
 
@@ -68,21 +69,21 @@ const LongtermWeather = (props) => {
       lon: currentPosition.lon,
       units: temperatureContext.unit,
       exclude: 'alerts,hourly,minutely',
-      appid: process.env.REACT_APP_WEATHER_API_KEY,
+      appid: process.env.REACT_APP_WEATHER_API_KEY
     });
     const dailyWeatherApiUrl = `https://api.openweathermap.org/data/2.5/onecall?${dailyWeatherQueryParams.toString()}`;
     const hourlyWeatherQueryParams = new URLSearchParams({
       q: cityContext,
       units: temperatureContext.unit,
       cnt: 8,
-      appid: process.env.REACT_APP_WEATHER_API_KEY,
+      appid: process.env.REACT_APP_WEATHER_API_KEY
     });
     const hourlyWeatherApiUrl = `http://api.openweathermap.org/data/2.5/forecast/?${hourlyWeatherQueryParams.toString()}`;
     fetch(isDailyWeather ? hourlyWeatherApiUrl : dailyWeatherApiUrl)
-      .then((response) => {
+      .then(response => {
         return response.json();
       })
-      .then((apiReply) => {
+      .then(apiReply => {
         const weatherData = isDailyWeather
           ? formatHourlyWeatherData(apiReply)
           : formatDailyWeatherData(apiReply);
@@ -91,25 +92,25 @@ const LongtermWeather = (props) => {
       });
   }, [cityContext, locationContext, temperatureContext, props.days]);
 
+   if (loadingState) {
+    return <CircularProgress color='secondary'/>;
+  }
+
   // check if props is undefined
   if (!longtermWeatherState || longtermWeatherState.length === 0) {
     return <p>Not able to fetch that weather</p>;
   }
 
+ 
+
   return (
     <>
-    {/* // change title of page based on amount of days  */}
-      {isDailyWeather ? (
-        <h1>24 HOUR PROGNOSIS</h1>
-      ) : (
-        <h1> {props.days} DAY PROGNOSIS</h1>
-      )}
+      {/* // change title of page based on amount of days  */}
+      {isDailyWeather ? <h1>24 HOUR PROGNOSIS</h1> : <h1> {props.days} DAY PROGNOSIS</h1>}
 
       {/* only pull info on selected amount of days  */}
-      {longtermWeatherState.slice(0, weatherCount).map((weatherInfo) => (
-        <>
-          <Accordion key={weatherInfo.id} weatherData={weatherInfo} ></Accordion>
-        </>
+      {longtermWeatherState.slice(0, weatherCount).map(weatherInfo => (
+        <Accordion key={weatherInfo.id} weatherData={weatherInfo}></Accordion>
       ))}
     </>
   );
